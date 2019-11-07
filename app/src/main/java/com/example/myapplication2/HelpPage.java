39a00b8db5385e390d55d62e03cb8b954da9bf78
Stephen Brien <x17141729@student.ncirl.ie>
2019-11-07 12:53:21 +0000

    Re-pushing the files, the Help page and the xml to go with it should now be included.

---
+++ app/src/main/java/com/example/myapplication2/HelpPage.java
@@ -0,0 +1,15 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_page2);
+    }
+//I'm just adding this comment to try to push this page up to github
+
+}
